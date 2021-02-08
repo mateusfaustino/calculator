a779ef910eac9322d736aea5c08d6d3933f91c3c
Mateus Faustino <mateusfaustino2017@gmail.com>
2021-02-08 20:38:30 -0300

    Second commit

---
+++ Desktop/calculator/application/src/index.js
@@ -0,0 +1,8 @@
+/*| Importar |------------------------------------------------------------------*/
+import React from 'react'
+import ReactDOM from 'react-dom'
+
+const elemento = <h1>Calculadora</h1>
+
+/*| Escrever na div #root do index.html |------------------------------------------------------------------*/
+ReactDOM.render(elemento,document.getElementById('root'))
